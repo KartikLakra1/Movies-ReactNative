4b32ba7f54332170080a67bff41c70b730182d4f
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-03-24 15:40:16 +0530

    layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
 const _layout = () => {
   return (
@@ -9,7 +11,17 @@ const _layout = () => {
             name='index'
             options={{
                 title: "Home",
-                headerShown : false
+                headerShown : false,
+                tabBarIcon : ({focused})=>(
+                    <>
+                        <ImageBackground source={images.highlight} className='flex flex-row w-full flex-1 min-h-14 min-w-[112px] mt-4 justify-center items-center rounded-full overflow-hidden'>
+                            <Image source={icons.home} tintColor={"#151312"} className='size-5'/>
+                            <Text>Home</Text>
+                            
+                        </ImageBackground>
+                        
+                    </>
+                )
             }}
         />
 
