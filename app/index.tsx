817424ab7a9f5b680389e6d77ae483e1e002ed99
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-03-24 14:54:08 +0530

    routing

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-slate-950">
       <Text className="text-5xl text-center font-bold text-light-300">Welcome to the movie app</Text>
+      
     </View>
   );
 }
