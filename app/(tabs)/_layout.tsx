57458723bec3e9410f21d142c4c5f18b75bd16ce
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-03-24 16:04:36 +0530

    navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,18 +5,50 @@ import { images } from '@/constants/images'
 import { icons } from '@/constants/icons'
 
 const TabIcon = ({focused , icon , title} :  any) => {
-    return (<ImageBackground source={images.highlight} className='flex flex-row w-full flex-1 min-h-14 min-w-[100] mt-4 justify-center items-center rounded-full overflow-hidden'>
-        <Image source={icon} tintColor={"#151312"} className='size-5'/>
-         <Text className='text-secondary text-base font-semibold'>{title}</Text>                    
-    </ImageBackground>
-    )
+
+    if(focused){
+        return (<ImageBackground source={images.highlight} className='flex flex-row w-full flex-1 min-h-14 min-w-[100] mt-4 justify-center items-center rounded-full overflow-hidden'>
+            <Image source={icon} tintColor={"#151312"} className='size-5'/>
+             <Text className='text-secondary text-base font-semibold'>{title}</Text>                    
+        </ImageBackground>
+        )
+
+        return (
+            <View className='size-full justify-center items-center mt-4 rounded-full'>
+
+                <Image source={icon} tintColor={"#ABB5DB"} className='size-5'/>
+            </View>
+        )
+    }
+    
 }
 
 const _layout = () => {
     
 
   return (
-    <Tabs>
+    <Tabs
+    
+        screenOptions={{
+            tabBarShowLabel:false,
+            tabBarItemStyle:{
+                width:'100%',
+                height:'100%',
+                justifyContent: 'center',
+                alignItems:'center'
+            },
+            tabBarStyle:{
+                backgroundColor: "#0f0D23",
+                borderRadius :50,
+                marginBottom:36,
+                height: 52,
+                position:'absolute',
+                overflow : 'hidden',
+                borderWidth : 1,
+                borderColor: "#0f0D23"
+            }
+        }}
+    >
         <Tabs.Screen 
             name='index'
             options={{
