dafaba50701276a903430767c0067990f5a55968
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2025-03-24 14:48:48 +0530

    tailwind.confid colors extend

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-center font-bold text-blue-500">Welcome to the movie app</Text>
+    <View className="flex-1 justify-center items-center bg-slate-950">
+      <Text className="text-5xl text-center font-bold text-light-300">Welcome to the movie app</Text>
     </View>
   );
 }
